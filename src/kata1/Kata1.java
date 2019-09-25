28a5cd337e8e73d4b9227a2d9a345a77f3bad816
Usuario <Usuario@10.22.148.87>
2019-09-25 14:07:16 +0100

    Project created and added Person class

---
+++ src/kata1/Kata1.java
@@ -0,0 +1,12 @@
+package kata1;
+
+import java.util.Date;
+
+public class Kata1 {
+
+    public static void main(String[] args) {
+        Person pau = new Person("Pau", new Date(80,10,1));
+        System.out.println(pau);
+    }
+    
+}
